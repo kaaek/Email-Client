d49d718c2c720dc5a57190c06a130044a5abd956
Khalil El Kaaki <khalil.el.kaaki@gmail.com>
2025-08-16 13:08:10 +0300

    MS2 Supports sending e-mails

---
+++ src/main/java/com/example/controller/EmailController.java
@@ -0,0 +1,26 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.dto.EmailRequest;
+import com.example.service.EmailService;
+
+@RestController
+@RequestMapping("/send-mail")
+public class EmailController {
+    
+    private final EmailService emailService;
+
+    public EmailController (EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @PostMapping
+    public void sendEmail(@RequestBody EmailRequest emailRequest) {
+        emailService.sendEmail(emailRequest.getEmail(), emailRequest.getMessage());
+    }
+
+}
